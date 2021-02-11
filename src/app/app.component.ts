119de4a50876f19433cbc3958c499495412b4fce
Shijin Elza Alex <shijinalex123@gmail.com>
2021-02-11 10:26:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ResultsDisplayService } from './results-display.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  term: any;
+  element:any;
+  details:any = {};
+  searchMatch: any;
+  buttonPressed = false;
+  constructor(private resultsDisplayService: ResultsDisplayService){}
+
+  onSubmit(){
+    this.element = document.getElementById('search');
+    if(this.element.value){
+      this.term = this.element.value;
+    }
+    this.buttonPressed = true;
+    this.resultsDisplayService.getWikiDetails(this.term)
+      .subscribe(res => {
+      this.details = res;
+      this.details = this.details.query.search;
+      console.log(this.details);
+    });
+ 
+  }
   title = 'WikiSearch';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+// import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
